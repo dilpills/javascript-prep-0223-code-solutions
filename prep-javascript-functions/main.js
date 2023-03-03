a4379ea01d83f92612074fb5b215c4b8b231bdc2
Dylan Dao <dylanpdao@gmail.com>
2023-03-03 04:30:22 +0000

    finished exercises

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,18 @@
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(addTwoNumbers(2, 2));
+
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+
+console.log(convertHoursToMinutes(2));
+
+function getGreeting(name) {
+  const greeting = 'Hello ' + name;
+  return greeting;
+}
+
+console.log(getGreeting('World!'));
