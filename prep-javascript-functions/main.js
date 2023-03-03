9962002cae9e75a4aad99b3150ed0081a0d85c2b
Dylan Dao <dylanpdao@gmail.com>
2023-03-03 04:39:58 +0000

    didnt see rest of exercises

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -16,3 +16,37 @@ function getGreeting(name) {
 }
 
 console.log(getGreeting('World!'));
+
+function addAndMultiplyBy5(num1, num2) {
+  const answer = (num1 + num2) * 5;
+  return answer;
+}
+
+console.log(addAndMultiplyBy5(10, 5));
+
+function multiplyAndDivideBy5(num1, num2) {
+  const answer = (num1 * num2) / 5;
+  return answer;
+}
+
+console.log(multiplyAndDivideBy5(35, 10));
+
+function subtractTwoNumbers(num1, num2) {
+  const answer = num1 - num2;
+  return answer;
+}
+
+console.log(subtractTwoNumbers(22, 7));
+
+function getCircleCircumference(radius) {
+  const answer = 2 * radius * 3.14;
+  return answer;
+}
+
+console.log(getCircleCircumference(5));
+
+function getFullName(firstName, lastName) {
+  return firstName + ' ' + lastName;
+}
+
+console.log(getFullName('Juan', 'Ramirez'));
