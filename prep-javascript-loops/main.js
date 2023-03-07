388a68568124b7eb94d9d7121068b5ab535cff0b
Dylan Dao <dylanpdao@gmail.com>
2023-03-07 07:53:36 +0000

    finished

--- prep-javascript-loops/main.js
+++ prep-javascript-loops/main.js
@@ -1,1 +1,81 @@
 // create your loops here.
+function whileLoop1() {
+  let i = 0;
+  const arr = [];
+
+  while (i < 10) {
+    arr.push(i);
+    i++;
+  }
+  return arr;
+}
+console.log('whileLoop1 output', whileLoop1());
+
+function whileLoop2() {
+  let i = 0;
+  const arr = [];
+
+  while (i < 20) {
+    arr.push(i);
+    i += 2;
+  }
+  return arr;
+}
+
+console.log('whileLoop2 output', whileLoop2());
+
+function forLoop1() {
+  const arr = [];
+
+  for (let i = 0; i < 10; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
+
+console.log('forLoop1 output', forLoop1());
+
+function forLoop2() {
+  const arr = [];
+
+  for (let i = 100; i > 0; i--) {
+    console.log('Time till explosion ' + (i));
+  }
+  return arr;
+}
+
+console.log('forLoop2 output', forLoop2());
+
+function forInLoop1(object) {
+  const obj = {
+    name: 'Ada LoveLace',
+    age: 'classic',
+    hobby: 'computation',
+    invention: 'analytical engine'
+  };
+  const arr = [];
+
+  for (const x in obj) {
+    // arr.push(obj[x]);
+    arr.push(x);
+  }
+  return arr;
+}
+console.log('forInLoop1 output', forInLoop1());
+
+function forInLoop2(object) {
+  const obj = {
+    name: 'Ada LoveLace',
+    age: 'classic',
+    hobby: 'computation',
+    invention: 'analytical engine'
+  };
+  const arr = [];
+
+  for (const x in obj) {
+    arr.push(obj[x]);
+    // arr.push(x);
+  }
+  return arr;
+}
+console.log('forInLoop2 output', forInLoop2());
--- prep-javascript-loops/prep-javascript-loops-quiz-notes.md
+++ prep-javascript-loops/prep-javascript-loops-quiz-notes.md
@@ -7,17 +7,17 @@
 After completing this exercise, you should be able to discuss or answer the following questions:
 
 - What is the purpose of the **condition** expression in the `for` and `while` loops?
-
+determines to run code or not, need to meet the requrment to run code
 - When is the first expression in the parentheses for a `for` loop (known as the **initialization**) evaluated?
-
+executed (one time) before the execution of the code block.
 - When is the second expression in the parentheses for a `for` loop (known as the **condition**) evaluated?
-
+defines the condition for executing the code block
 - When is the third expression in the parentheses for a `for` loop (known as the **final expression**) evaluated?
-
+executed (every time) after the code block has been executed
 - What is the purpose of the **final expression** in a `for` loop?
-
+to stop the loop
 - What types of data should the `for...in` loop be used on?
-
+only iterates over enumerable, non-symbol properties
 
 
 ## Notes
